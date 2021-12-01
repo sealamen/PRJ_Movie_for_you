31366620e1b2c3fc65edc7e619360413343900a4
sealamen <wltjrdlqns@naver.com>
2021-12-01 16:13:40 +0900

    First commit

--- job01_crawling.py
+++ job01_crawling.py
@@ -1,1 +1,13 @@
-print('git test')
+# crawling 작업
+
+# crawling은 각자 진행하고 빨리 완성되는 코드로 연도를 나눠서 진행
+# 2020년 개봉작만 크롤링, 저장형식 csv
+# 나머지는 연도별로 나눠서 크롤링해서 합치기
+# 컬럼명은 ['title','reviews']로 통일해주세요
+# 파일명은 reviews_0000.csv로 해주세요. 0000은 해당연도입니다.
+# 크롤링한 데이터 파일은 아래 링크로 올려주세요.
+# https://drive.google.com/drive/folders/1vICarPIiRxa65pItqwBEwvMD_tlf0xQw?usp=sharing
+
+
+# 보통 이런 식으로 요청이 들어와
+
